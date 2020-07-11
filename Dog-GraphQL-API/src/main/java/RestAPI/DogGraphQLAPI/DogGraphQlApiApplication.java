e684af0410801b859ab0daa16ef47d94bd78b5e3
Quan Le <qle2@hawk.iit.edu>
2020-07-11 00:04:38 -0500

    no message

---
+++ Dog-GraphQL-API/src/main/java/RestAPI/DogGraphQLAPI/DogGraphQlApiApplication.java
@@ -0,0 +1,13 @@
+package RestAPI.DogGraphQLAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogGraphQlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogGraphQlApiApplication.class, args);
+	}
+
+}
---
+++ Dog-GraphQL-API/src/test/java/RestAPI/DogGraphQLAPI/DogGraphQlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package RestAPI.DogGraphQLAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogGraphQlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
