edf3ecb3c09f86d372177b311ac70ea560967e86
Quan Le <qle2@hawk.iit.edu>
2020-07-13 08:42:52 -0500

    no message

---
+++ Dog-GraphQL-API/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO dog (id, name, breed, origin, owner) VALUES (01, 'Dug', 'Golden Retriever', 'Up, Disney Pixar', 'Charles Muntz');
+INSERT INTO dog (id, name, breed, origin, owner) VALUES (02, 'Spike', 'Grey American Bulldog', 'Tom and Jerry, Metro Goldwyn Mayer', 'NAME NOT FOUND');
+INSERT INTO dog (id, name, breed, origin, owner) VALUES (03, 'Tyke', 'Grey American Bulldog', 'Tom and Jerry, Metro Goldwyn Mayer', 'NAME NOT FOUND');
+INSERT INTO dog (id, name, breed, origin, owner) VALUES (04, 'Buck', 'St. Bernard/Scotch Collie', 'The Call of the Wild, Jack London', 'Judge Miller/John Thornton');
+INSERT INTO dog (id, name, breed, origin, owner) VALUES (05, 'Hachikō', 'Akita Inu', 'Akita Prefecture, Japan', 'Hidesaburō Ueno');
+INSERT INTO dog (id, name, breed, origin, owner) VALUES (06, 'Pluto', 'Bloodhound', 'Mickey, Disney Pixar', 'Mickey Mouse');
