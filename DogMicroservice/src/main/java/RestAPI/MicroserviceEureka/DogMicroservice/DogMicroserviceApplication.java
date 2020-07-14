20c20eca2ad50b4ff4686aa6a5ac747d1407d8ac
Quan Le <qle2@hawk.iit.edu>
2020-07-14 13:24:03 -0500

    no message

--- DogMicroservice/src/main/java/RestAPI/MicroserviceEureka/DogMicroservice/DogMicroserviceApplication.java
+++ DogMicroservice/src/main/java/RestAPI/MicroserviceEureka/DogMicroservice/DogMicroserviceApplication.java
@@ -2,8 +2,10 @@ package RestAPI.MicroserviceEureka.DogMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DogMicroserviceApplication {
 
 	public static void main(String[] args) {
