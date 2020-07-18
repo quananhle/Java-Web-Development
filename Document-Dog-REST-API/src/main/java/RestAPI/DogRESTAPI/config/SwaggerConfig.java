9fcaaac824afed46b779393cb9287cfcee64471c
Quan Le <qle2@hawk.iit.edu>
2020-07-17 22:54:00 -0500

    Fixed SwaggerConfig

--- Document-Dog-REST-API/src/main/java/RestAPI/DogRESTAPI/config/SwaggerConfig.java
+++ Document-Dog-REST-API/src/main/java/RestAPI/DogRESTAPI/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package RestAPI.DogRESTAPI.config;
 
+import com.google.common.base.Predicate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -21,8 +23,8 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .apis((Predicate<RequestHandler>) RequestHandlerSelectors.any())
+                .paths((Predicate<String>) PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
     }
