c0027044f71a4fa3c49f0d30b1a238cd6528c2fc
Quan Le <qle2@hawk.iit.edu>
2020-07-24 09:43:41 -0500

    Modified JWTAuthenticationFilter

--- eCommerce_Application/src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
+++ eCommerce_Application/src/main/java/com/example/demo/security/JWTAuthenticationFilter.java
@@ -1,4 +1,53 @@
 package com.example.demo.security;
 
-public class JWTAuthenticationFilter {
+import com.auth0.jwt.JWT;
+import com.example.demo.model.persistence.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+        throws AuthenticationException {
+        try {
+            User credentials = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            return authenticationManager.authenticate
+                    (new UsernamePasswordAuthenticationToken(credentials.getUsername(),
+                                                             credentials.getPassword(),
+                            new ArrayList<>()));
+        } catch (IOException ioe) {
+            throw new RuntimeException(ioe);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain filterChain,
+                                            Authentication authentication)
+        throws IOException, ServletException {
+        String token = JWT.create()
+                .withSubject(((org.springframework.security.core.userdetails.User) authentication.getPrincipal()).getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
+                        .sign(HMAC512(SecurityConstants.SECRET.getBytes()));
+        request.addHeader(SecurityConstants.HEADER_STRING, SecurityConstants.TOKEN_PREFIX + token);
+    }
 }
