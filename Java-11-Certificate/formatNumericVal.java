b24463fbc99b2459ee2ad34a4407ae6b7e0c859c
Quan Le <qle2@hawk.iit.edu>
2020-08-27 10:24:51 -0500

    no message

--- Java-11-Certificate/formatNumericVal.java
+++ Java-11-Certificate/formatNumericVal.java
@@ -7,11 +7,13 @@ public class Main {
         var intF = NumberFormat.getIntegerInstance();
         System.out.println("Number: " + intF.format(doubleValue));
         intF.setGroupingUsed(false);
-        System.out.println("Number: " + intF.format(doubleValue);
+        System.out.println("Number: " + intF.format(doubleValue));
         var locale = new Locale("de", "DE");
         VAR localeFormatter = NumberFormat.getNumberInstance(locale);
         System.out.println("Number: " + localeFormatter.format(doubleValue));
         var currencyFormatter = NumberFormat.getCurrencyInstance(locale);
-        System.out.prinln(currencyFormatter);
+        System.out.prinln(currencyFormatter.format(doubleValue));
+        var df = new DecimalFormat("$00.00");
+        System.out.println(df.format(1));
     }
 }
