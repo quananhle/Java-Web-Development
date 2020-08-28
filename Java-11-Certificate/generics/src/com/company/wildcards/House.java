f069c5d206f3f29c140a972c49aa3f9befaf914a
Quan Le <qle2@hawk.iit.edu>
2020-08-28 00:35:51 -0500

    generics

--- Java-11-Certificate/generics/src/com/company/wildcards/House.java
+++ Java-11-Certificate/generics/src/com/company/wildcards/House.java
@@ -1,8 +1,16 @@
 package com.company.wildcards;
 
 public class House extends Building {
+    public int numberRooms = 3;
+
     @Override
     public String toString() {
         return ("House{}");
+    }
+    public int getNumberRooms() {
+        return numberRooms;
+    }
+    public void setNumberRooms(int numberRooms) {
+        this.numberRooms = numberRooms;
     }
 }
---
+++ Java-11-Certificate/generics/src/com/company/wildcards/Office.java
@@ -0,0 +1,8 @@
+package com.company.wildcards;
+
+public class Office extends Building{
+    @Override
+    public String toString() {
+        return ("Office{}");
+    }
+}
