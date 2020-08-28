6a28c509a35002b0823a319e055edf1762cd1dbe
Quan Le <qle2@hawk.iit.edu>
2020-08-28 00:29:06 -0500

    generics

--- Java-11-Certificate/generics/src/com/company/Main.java
+++ Java-11-Certificate/generics/src/com/company/Main.java
@@ -15,10 +15,20 @@ public class Main {
         return list;
     }
 
+    //varargs method
+    public static void printShoppinglist(String... items) {
+        System.out.println("SHOPPING LIST");
+        for (int i = 0; i < items.length; i++) {
+            System.out.println(i+1 + ": " + items[i]);
+        }
+        System.out.println("\n");
+    }
+
     public static void main(String[] args) {
 	    List<Character> characterList = arrayToList(charArray, new ArrayList<>());
         List<Boolean> booleanList = arrayToList(boolArray, new ArrayList<>());
         List<Integer> integerList = arrayToList(intArray, new ArrayList<>());
         System.out.println(integerList.get(0));
+        printShoppinglist("Bread", "Milk", "Eggs", "Bacon", "Veggie");
     }
 }
