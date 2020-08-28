c31b5ca243eea17b2240bf2649d2d6c572308d08
Quan Le <qle2@hawk.iit.edu>
2020-08-28 00:04:52 -0500

    generics

--- Java-11-Certificate/generics/src/com/company/Main.java
+++ Java-11-Certificate/generics/src/com/company/Main.java
@@ -1,6 +1,10 @@
 package com.company;
 
-public class Main {
+public class GenericMethods {
+    static Character[] charArray = {'h', 'e', 'l', 'l', 'o'};
+    static Integer[] intArray = {1, 2, 3, 4, 5};
+    static Boolean[] boolArray = {true, false, true};
+
 
     public static void main(String[] args) {
 	// write your code here
