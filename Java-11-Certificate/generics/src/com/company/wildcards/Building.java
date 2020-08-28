f1c17283c00d023cda4807c23b1fab7fd2c9e57a
Quan Le <qle2@hawk.iit.edu>
2020-08-28 00:32:19 -0500

    generics

--- Java-11-Certificate/generics/src/com/company/wildcards/Building.java
+++ Java-11-Certificate/generics/src/com/company/wildcards/Building.java
@@ -1,4 +1,17 @@
 package com.company.wildcards;
 
 public class Building {
+    public int numberRooms = 7;
+
+    @Override
+    public String toString() {
+        return ("Building");
+    }
+
+    public int getNumberRooms() {
+        return numberRooms;
+    }
+    public void setNumberRooms(int numberRooms) {
+        this.numberRooms = numberRooms;
+    }
 }
