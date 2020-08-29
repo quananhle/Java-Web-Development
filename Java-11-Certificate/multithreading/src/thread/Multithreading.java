28982c852e9885e7d59acdb3f993caaa5a0050c8
Quan Le <qle2@hawk.iit.edu>
2020-08-29 01:33:12 -0500

    Multithreading

--- Java-11-Certificate/multithreading/src/thread/Multithreading.java
+++ Java-11-Certificate/multithreading/src/thread/Multithreading.java
@@ -3,6 +3,11 @@ package thread;
 public class Multithreading {
 
     public static void main(String[] args) {
-        ThreadExample thread1 = new ThreadExample;
+        ThreadExample thread1 = new ThreadExample();
+        thread1.setName("First thread");
+        thread1.start();
+        ThreadExample thread2 = new ThreadExample();
+        thread2.setName("Second thread");
+        thread2.start();
     }
 }
---
+++ Java-11-Certificate/multithreading/src/thread/ThreadExample.java
@@ -0,0 +1,12 @@
+package thread;
+
+public class ThreadExample extends Thread {
+    @Override
+    public void run() {
+        int i = 1;
+        while(i <= 100) {
+            System.out.println(i + " " + this.getName());
+            i++;
+        }
+    }
+}
