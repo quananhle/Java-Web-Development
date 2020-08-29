9c4b9cbe99178216383eb28c0077e36a07d4f785
Quan Le <qle2@hawk.iit.edu>
2020-08-29 00:21:34 -0500

    Add streams

--- Java-11-Certificate/streams/src/com/company/Book.java
+++ Java-11-Certificate/streams/src/com/company/Book.java
@@ -1,4 +1,32 @@
 package com.company;
 
 public class Book {
+    private String author;
+    private String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return ("Author: " + author + " Title: " + title);
+    }
 }
