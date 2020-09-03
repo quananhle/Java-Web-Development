16301c59a1dc1c7437d16cbbd4cff9e769cfdaf4
Quan Le <qle2@hawk.iit.edu>
2020-09-03 12:09:27 -0500

    no message

---
+++ Cucumber/cucumber/src/main/java/Maven_Cucumber_BDD/cucumber/App.java
@@ -0,0 +1,13 @@
+package Maven_Cucumber_BDD.cucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
